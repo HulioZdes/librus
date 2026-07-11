73469c8047f8abd2f1e591f1cefa8de4154c332a
Jay <jmartblues@ukr.net>
2026-07-11 16:19:37 +0300

    Create sample_data.py with book instances
    Added a list of book instances with titles, authors, pages, and ratings.

---
+++ sample_data.py
@@ -0,0 +1,32 @@
+from book import Book
+
+books = [
+
+    Book(
+        "Clean Code",
+        "Robert C. Martin",
+        464,
+        5
+    ),
+
+    Book(
+        "Atomic Habits",
+        "James Clear",
+        320,
+        5
+    ),
+
+    Book(
+        "Deep Work",
+        "Cal Newport",
+        304,
+        4
+    ),
+
+    Book(
+        "Python Crash Course",
+        "Eric Matthes",
+        388,
+        5
+    )
+]
