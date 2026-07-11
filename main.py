75c4a3a2c23698f2dc85ab93c097fa6c30f11e26
Jay <jmartblues@ukr.net>
2026-07-11 16:16:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-""" Book collection manager """
+from library import Library
+from sample_data import books
+from statistics import show_statistics
+from search import find_by_title
 
-print("Project: book-library")
+library = Library()
+
+for book in books:
+    library.add(book)
+
+library.show()
+
+show_statistics(library.books)
+
+print("\nSearch Results:\n")
+
+results = find_by_title(
+    library.books,
+    "Python"
+)
+
+for book in results:
+    print(book)
+    print()
